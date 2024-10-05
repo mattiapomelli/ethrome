4acf75e3b588f4fe56bf9439fc733a1da6f7b1cf
stefanofa <9802152+stefanofa@users.noreply.github.com>
2024-10-05 16:23:04 +0200

    style: add bottom padding on create screen

--- src/components/ui/dock.tsx
+++ src/components/ui/dock.tsx
@@ -18,19 +18,21 @@ type DockProps = {
 
 export const Dock = ({ items, className }: DockProps) => {
   return (
-    <div
-      className={cn(
-        "fixed inset-x-5 bottom-5 z-50 mx-auto flex w-fit items-center justify-center gap-x-2 rounded-full bg-black/20 px-3 py-2 backdrop-blur-sm",
-        className,
-      )}
-    >
-      {items.map(({ title, icon, href }) => (
-        <Link key={title} href={href}>
-          <div className="relative flex aspect-square items-center justify-center rounded-full p-2 transition-colors duration-300 ease-out hover:bg-white/10 dark:hover:bg-neutral-800">
-            {icon}
-          </div>
-        </Link>
-      ))}
+    <div className="fixed inset-x-5 bottom-5 z-50">
+      <div
+        className={cn(
+          "mx-auto flex w-fit items-center justify-center gap-x-4 rounded-full bg-primary-foreground/10 px-3 py-2 backdrop-blur-sm",
+          className,
+        )}
+      >
+        {items.map(({ title, icon, href }) => (
+          <Link key={title} href={href}>
+            <div className="relative flex aspect-square items-center justify-center rounded-full p-2 transition-colors duration-300 ease-out hover:bg-white/10 dark:hover:bg-neutral-800">
+              {icon}
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
