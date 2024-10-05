39f3f7d6b5fe7b1d78120d064c9e5a7bd8fda337
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2024-10-05 13:58:47 +0200

    refactor: feeds page

--- src/app/(logged-in)/feed/page.tsx
+++ src/app/(logged-in)/feed/page.tsx
@@ -1,57 +1,15 @@
 "use client";
 
+import { useCasts } from "@/app/(logged-in)/feed/_hooks/use-casts";
 import { Cast } from "@/components/ui/cast/cast";
-import { CastWithInteractions, EmbedUrl } from "@neynar/nodejs-sdk/build/neynar-api/v2";
-import { usePrivy } from "@privy-io/react-auth";
-import { useCallback, useEffect, useState } from "react";
 
 const FeedPage = () => {
-  const [casts, setCasts] = useState<CastWithInteractions[]>([]);
-  const [loading, setLoading] = useState(true);
-  const { user } = usePrivy();
+  const { data: casts, isLoading } = useCasts();
 
-  const fetchFeed = useCallback(async () => {
-    if (!user || !user.farcaster) return;
+  if (isLoading) return <div>loading...</div>;
+  if (!casts) return <div>No casts</div>;
 
-    setLoading(true);
-
-    const viewerFid = user.farcaster.fid;
-
-    const response = await fetch(`/api/feed?viewerFid=${viewerFid}`);
-
-    if (!response.ok) {
-      console.error("Failed to fetch feed");
-      return;
-    }
-
-    const data = await response.json();
-
-    console.log(data);
-
-    setCasts(data);
-    setLoading(false);
-  }, [user]);
-
-  useEffect(() => {
-    fetchFeed();
-  }, [fetchFeed]);
-
-  return (
-    <div className="size-full flex-1">
-      <div className="text-2xl font-bold">feed page</div>
-      {loading && <div>loading...</div>}
-      {!loading && (
-        <Cast className="flex flex-col gap-4">
-          {casts.map((cast) => (
-            <div key={cast.hash}>
-              <div>text: {cast.text}</div>
-              <div>url: {(cast.embeds?.[0] as EmbedUrl)?.url}</div>
-            </div>
-          ))}
-        </Cast>
-      )}
-    </div>
-  );
+  return casts.map((cast) => <Cast key={cast.hash} imgSrc="" />);
 };
 
 export default FeedPage;
