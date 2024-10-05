c0daded41efd96c58102b263807bd189e46128bd
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2024-10-05 18:41:48 +0200

    feat: unlock content button

--- src/app/(logged-in)/feed/page.tsx
+++ src/app/(logged-in)/feed/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { EmbeddedCast, EmbedUrl } from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import { LockOpen } from "lucide-react";
 import { Hex } from "viem";
 
 import { useCasts } from "@/app/(logged-in)/feed/_hooks/use-casts";
 import { Cast, UserInfo } from "@/components/ui/cast/cast";
 import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
 
 const isEmbedUrl = (embed: EmbeddedCast): embed is EmbedUrl => {
   return (embed as EmbedUrl).url !== undefined;
@@ -37,7 +39,19 @@ const FeedPage = () => {
       username: cast.text,
     };
 
-    return <Cast key={cast.hash} imgSrc={embeds[0].previewImg} userInfo={userInfo} />;
+    return (
+      <>
+        <Cast key={cast.hash} imgSrc={embeds[0].previewImg} userInfo={userInfo} />
+        <button
+          className={cn(
+            "absolute bottom-40 right-2 z-50 flex aspect-square items-center justify-center rounded-full border border-primary p-2 transition-colors duration-300 ease-out hover:bg-primary/20",
+          )}
+          onClick={() => console.log("unlock")}
+        >
+          <LockOpen className="size-4 text-primary" />
+        </button>
+      </>
+    );
   });
 };
 
