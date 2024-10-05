d7c6e2dfc18030cb24a4eb82ab4a826a995207d3
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2024-10-05 16:35:38 +0200

    feat: parse cast embeds

--- src/app/(logged-in)/feed/page.tsx
+++ src/app/(logged-in)/feed/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import { EmbeddedCast, EmbedUrl } from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import { Hex } from "viem";
+
 import { useCasts } from "@/app/(logged-in)/feed/_hooks/use-casts";
-import { Cast } from "@/components/ui/cast/cast";
+import { Cast, UserInfo } from "@/components/ui/cast/cast";
+
+const isEmbedUrl = (embed: EmbeddedCast): embed is EmbedUrl => {
+  return (embed as EmbedUrl).url !== undefined;
+};
 
 const FeedPage = () => {
   const { data: casts, isLoading } = useCasts();
@@ -9,7 +16,28 @@ const FeedPage = () => {
   if (isLoading) return <div>loading...</div>;
   if (!casts) return <div>No casts</div>;
 
-  return casts.map((cast) => <Cast key={cast.hash} imgSrc="" />);
+  return casts.map((cast) => {
+    const embeds = cast.embeds.map((embed) => {
+      try {
+        if (isEmbedUrl(embed)) {
+          const parsedEmbed = JSON.parse(embed.url);
+          return parsedEmbed;
+        }
+      } catch (error) {
+        console.error("Failed to parse embed:", error);
+      }
+
+      return null;
+    });
+
+    if (!embeds[0]?.previewImg) return null;
+    const userInfo: UserInfo = {
+      address: cast.author.custody_address as Hex,
+      username: cast.text,
+    };
+
+    return <Cast key={cast.hash} imgSrc={embeds[0].previewImg} userInfo={userInfo} />;
+  });
 };
 
 export default FeedPage;
