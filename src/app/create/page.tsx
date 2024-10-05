60cda9f3e64804be0ab3848b235a1da2e4dd2474
stefanofa <9802152+stefanofa@users.noreply.github.com>
2024-10-05 10:00:47 +0200

    feat: add create and feed pages

---
+++ src/app/create/page.tsx
@@ -0,0 +1,5 @@
+const CreatePage = () => {
+  return <div className="size-full flex-1">create page</div>;
+};
+
+export default CreatePage;
---
+++ src/app/feed/page.tsx
@@ -0,0 +1,5 @@
+const FeedPage = () => {
+  return <div className="size-full flex-1">feed page</div>;
+};
+
+export default FeedPage;
