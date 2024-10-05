16a5df044fcbdeda6bd3e7d6cb229682c85474ea
Gabriele Di Remigio <diremigiogabriele@gmail.com>
2024-10-05 12:22:18 +0200

    feat: add cast content UI

--- src/components/ui/cast/cast.tsx
+++ src/components/ui/cast/cast.tsx
@@ -1,22 +1,49 @@
-"use client";
-
+import Image from "next/image";
 import { HTMLAttributes } from "react";
 
 import { cn } from "@/lib/utils";
 
-type CastProps = {
-  className?: string;
+type UserInfo = {
+  avatarImgSrc: string;
+  username: string;
 };
 
-export const Cast = ({ className, children }: CastProps & HTMLAttributes<HTMLDivElement>) => {
+type CastProps = {
+  userInfo?: UserInfo;
+  imgSrc: string;
+  description?: string;
+  className?: string;
+} & HTMLAttributes<HTMLDivElement>;
+
+export const Cast = ({ userInfo, imgSrc, description, className, ...props }: CastProps) => {
   return (
     <div
       className={cn(
-        "z-10 flex h-screen w-full flex-none snap-center items-center justify-center",
+        "relative z-10 flex h-screen w-full flex-none snap-center items-center",
         className,
       )}
+      {...props}
     >
-      {children}
+      {/* User Information */}
+      {userInfo && (
+        <div className="absolute inset-x-2 bottom-20 z-20 flex max-w-md flex-col gap-y-2 rounded-sm bg-black/20 p-2 backdrop-blur-sm">
+          <div className="flex items-center justify-start gap-x-2">
+            <Image
+              src={userInfo.avatarImgSrc}
+              alt={`${userInfo.username} profile image`}
+              className="size-6 rounded-full"
+              width={24}
+              height={24}
+            />
+            <h2 className="text-sm font-semibold">{userInfo.username}</h2>
+          </div>
+
+          <p className="max-h-10 overflow-y-scroll text-sm font-light">{description}</p>
+        </div>
+      )}
+
+      {/* Cast Content */}
+      <Image src={imgSrc} alt="Cast Image" fill objectFit="cover" className="absolute" />
     </div>
   );
 };
